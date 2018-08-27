af95b95c3bf42b1fc96b72d0d7a01b5ed55c2dac
Justice Arthur <justicea83@gmail.com>
2018-08-27 16:12:54 +0100

    S commit

--- ManageCredentials/Controllers/new 1.php
+++
@@ -1,6 +0,0 @@
-
-for ($i=0; $i < count($studentPositionArray); $i++) {
-        if($studentPositionArray[$i][1] == studentid){
-			array_push($FinalstudentPosArray,$studentPositionArray[$i])
-		}
-    }
